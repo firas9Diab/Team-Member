5c52afdeb2a7ed3f274da48420ba2752a8cdbe3c
H. Eid <h.eid@penguinin.com>
2026-05-19 15:49:46 +0300

    sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import Item from "./components/Item/Item";
+import Sign from "./components/Sign/Sign";
+// import Item from "./components/Item/Item";
 
 const App = () => {
   return (
@@ -9,7 +10,9 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/item/:id" element={<Item />} />
+        {/* <Route path="/item/:id" element={<Item />} /> 
+       <Route path="/login" element={<Login />} />*/}
+        <Route path="/Sign" element={<Sign />} />
       </Routes>
     </div>
   );
