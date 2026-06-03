0dcebe12ee6b20928a3d2d3e239d7a0661e56c7f
M. Thaher <m.thaher@penguinin.com>
2026-06-03 09:15:36 +0300

    (Add user+Login+Sign up+Home) using .tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -17,79 +17,56 @@ interface User {
 const Home = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [totalPages, setTotalPages] = useState<number>(1);
-  const [clickpower, setClickPower] = useState<boolean>(false);
+  const[currentPage, setcurrentPage] = useState<number>(1);
 
-  const fetchUsers = async (page: number, filter: string, search: string) => {
-    const token = localStorage.getItem("token");
+const fetchUsers = async (
+  page: number = 1,
+  filter: string = selectedFilter,
+  searchValue: string = search
+) => {
+  const token = localStorage.getItem("token");
 
-    const params: any = {
-      limit: 5,
+  const response = await axios.get("http://localhost:3000/team-members", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      search: searchValue || undefined,
+      status:
+        filter === "Active"
+          ? "ACTIVE"
+          : filter === "Inactive"
+          ? "INACTIVE"
+          : undefined,
+      favoritesOnly: filter === "Favorites" ? true : undefined,
       page,
-    };
+      limit: 5,
+    },
+  });
 
-    if (search.trim() !== "") params.search = search;
+  const mapped = response.data.data.map((user: any) => ({
+    id: user.id,
+    name: user.fullName,
+    role: user.jobTitle,
+    status: user.status.toLowerCase(),
+    isFavorite: user.isFavorite ?? false,
+    avatar: user.avatarUrl,
+  }));
 
-    if (filter === "Favorites") params.favoritesOnly = true;
-    else if (filter === "Active") params.status = "ACTIVE";
-    else if (filter === "Inactive") params.status = "INACTIVE";
-
-    const response = await axios.get("http://localhost:3000/team-members", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params:
-      {
-        
-      },
-    });
-
-    const mapped = response.data.data.map((user: any) => ({
-      id: user.id,
-      name: user.fullName,
-      role: user.jobTitle,
-      status: user.status.toLowerCase(),
-      isFavorite: user.isFavorite ?? false,
-      avatar: user.avatarUrl,
-    }));
-
-    setUsers(mapped);
-    setTotalPages(response.data.meta.totalPages);
-  };
+  setUsers(mapped);
+  setTotalPages(response.data.meta.totalPages);
+};
 
   const [selectedFilter, setSelectedFilter] = useState<string>("All");
   const [search, setSearch] = useState<string>("");
-  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
-  const [currentPage, setSelectedPage] = useState<number>(1);
  
- const [totalpages, setTotalPages] = useState<number>(1);
+useEffect(() => {
+  setcurrentPage(1);
+}, [selectedFilter, search]);
 
-  useEffect(() => {
-    fetchUsers(1, "All", "");
-  }, []);
-
-  useEffect(() => {
-    const filtered = users.filter((person) => {
-      const matchSearch =
-        person.name.toLowerCase().includes(search.toLowerCase()) ||
-        person.role.toLowerCase().includes(search.toLowerCase());
-
-      if (selectedFilter === "All") return matchSearch;
-
-      if (selectedFilter === "Favorites")
-        return person.isFavorite && matchSearch;
-
-      if (selectedFilter === "Active")
-        return person.status === "active" && matchSearch;
-
-      if (selectedFilter === "Inactive")
-        return person.status === "inactive" && matchSearch;
-
-      return false;
-    });
-
-    setFilteredUsers(filtered);
-  }, [search, selectedFilter, users]);
-
+useEffect(() => {
+  fetchUsers(currentPage, selectedFilter, search);
+}, [currentPage, selectedFilter, search]);
   return (
     <>
       <Header count={users.length} />
@@ -99,14 +76,12 @@ const Home = () => {
           <FilterTabs
             selectedFilter={selectedFilter}
             setSelectedFilter={setSelectedFilter}
-            favoritesCount={favoritesCount}
-            activeCount={activeCount}
-            inactiveCount={inactiveCount}
+          
             allCount={users.length}
             search={search}
             fetchUsers={fetchUsers}
             selectedpage={currentPage}
-            setSelectedPage={setSelectedPage}
+            setSelectedPage={setcurrentPage}
           />
 
           <SearchInput
@@ -118,28 +93,30 @@ const Home = () => {
         </div>
 
         <div className={styles.container3}>
-          <UserList users={filteredUsers} />
+          <UserList users={users} />
         </div>
         <div className={styles.container4}>
           <ul className={styles.list}>
-            {pages.map((page) => (
-              <button
-                className={styles.pageButton}
-                onClick={() => {
-                  setSelectedPage(page);
-                  fetchUsers(page, selectedFilter, search);
-                  setClickPower(true);
-                }}
-                disabled={clickpower && selectedPage === page}
-                className={
-                  selectedPage === page
-                    ? styles.activePageButton
-                    : styles.pageButton
-                }
-              >
-                {page}
-              </button>
-            ))}
+       {new Array(totalPages).fill(0).map((_, i) => {
+ 
+
+  return (
+   <button
+  key={i + 1}
+  onClick={() => {
+    setcurrentPage(i + 1);
+  }}
+  disabled={currentPage === i + 1}
+  className={
+    currentPage === i + 1
+      ? styles.activePageButton
+      : styles.pageButton
+  }
+>
+  {i + 1}
+</button>
+  );
+})}
           </ul>
         </div>
       </div>
