4e58fce3eefd97f76e11608f80ee958072939e05
M. Thaher <m.thaher@penguinin.com>
2026-06-14 16:00:03 +0300

    passwords

--- src/components/User/Settings/Password/Password.tsx
+++ src/components/User/Settings/Password/Password.tsx
@@ -1,7 +1,81 @@
-import React from "react";
+import styles from "./Password.module.scss";
+import usePassword from "./usePassword";
+import eyeIcon from "../../../../../public/Icons/EyeIcon.svg";
 
 const Password = () => {
-  return <div>Password</div>;
+  const {visibleOldPassword,
+    visibleNewPassword,
+    visibleNewPasswordConfirm,
+    oldPassword,
+    newPassword,
+    confirmNewPassword,
+    error,
+    handleDataChange,
+    handleUpdatePassword,
+    handleVisibleChange
+  } = usePassword();
+
+  return (
+    <div className={styles.settingswork}>
+      <p className={styles.settingsworkparegraph}>
+        Update your personal details quickly and conveniently right here.Whether
+        you've got a new address, phone number, or just want to keep things
+        current, this is the place to do it.Keep your profile up-to-date
+        hassle-free.
+      </p>
+
+      <div className={styles.settingsinputfields}>
+        <div className={styles.inputfields}>
+          <label>Old Password</label>
+          <div className={styles.inputfield}>
+            <input
+              name="oldPassword"
+              type={visibleOldPassword?"text":"password"}
+              value={oldPassword}
+              onChange={handleDataChange}
+              className={styles.passwordfield}
+            />
+            <img onClick={()=>handleVisibleChange("oldPassword")} src={eyeIcon} alt="" />
+          </div>
+        </div>
+        <div className={styles.inputfields}>
+          <label>New Password</label>
+          <div className={styles.inputfield}>
+            <input
+              name="newPassword"
+                      type={visibleNewPassword?"text":"password"}
+              value={newPassword}
+              onChange={handleDataChange}
+              className={styles.passwordfield}
+            />
+            <img  onClick={()=>handleVisibleChange("newPassword")} src={eyeIcon} alt="" />
+          </div>
+        </div>
+        <div className={styles.inputfields}>
+          <label>Confirm New Password</label>
+          <div className={styles.inputfield}>
+            <input
+              name="confirmNewPassword"
+                       type={visibleNewPasswordConfirm?"text":"password"}
+              value={confirmNewPassword}
+              onChange={handleDataChange}
+              className={styles.passwordfield}
+            />
+            <img  onClick={()=>handleVisibleChange("confirmNewPassword")} src={eyeIcon} alt="" />
+          </div>
+
+          {error}
+        </div>
+
+        <button
+          className={styles.inputfieldbutton}
+          onClick={handleUpdatePassword}
+        >
+          Update Password
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Password;
---
+++ src/components/User/Settings/Password/usePassword.ts
@@ -0,0 +1,111 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const usePassword = () => {
+  const [oldPassword, setOldPassword] = useState<string>("");
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [visibleOldPassword, setVisibleOldPassword] = useState<boolean>(false);
+  const [visibleNewPassword, setVisibleNewPassword] = useState<boolean>(false);
+  const [visibleNewPasswordConfirm, setVisibleNewPasswordConfirm] =
+    useState<boolean>(false);
+
+
+  const handleVisibleChange = ( field: "oldPassword" | "newPassword" | "confirmNewPassword") =>
+  {
+     switch (field) {
+      case "oldPassword":
+        setVisibleOldPassword(!visibleOldPassword);
+        break;
+
+      case "newPassword":
+        setVisibleNewPassword(!visibleNewPassword);
+        break;
+
+      case "confirmNewPassword":
+        setVisibleNewPasswordConfirm(!visibleNewPasswordConfirm);
+        break;
+
+      default:
+        break;
+    }
+
+  }
+
+  const handleDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    switch (e.target.name) {
+      case "oldPassword":
+        setOldPassword(e.target.value);
+        break;
+
+      case "newPassword":
+        setNewPassword(e.target.value);
+        break;
+
+      case "confirmNewPassword":
+        setConfirmNewPassword(e.target.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleUpdatePassword = async () => {
+    if (!oldPassword || !newPassword || !confirmNewPassword) {
+      setError("All fields are required");
+      return;
+    }
+
+    if (oldPassword === newPassword) {
+      setError("New password must be different from old password");
+      return;
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      setError("New password and confirm password do not match");
+      return;
+    }
+
+    try {
+      const token = localStorage.getItem("token");
+
+      await axios.patch(
+        "http://localhost:3000/users/me/password",
+        {
+          oldPassword,
+          newPassword,
+          confirmNewPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      setError("Password updated successfully!");
+
+      setOldPassword("");
+      setNewPassword("");
+      setConfirmNewPassword("");
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Failed to update password");
+    }
+  };
+
+  return {visibleOldPassword,
+    visibleNewPassword,
+    visibleNewPasswordConfirm,
+    oldPassword,
+    newPassword,
+    confirmNewPassword,
+    error,
+    handleDataChange,
+    handleUpdatePassword,
+    handleVisibleChange
+  };
+};
+
+export default usePassword;
